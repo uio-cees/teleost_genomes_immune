1447345c6048a0b3a8f69c5ffde858af467bfb4b
Michael Matschiner <michaelmatschiner@mac.com>
2016-01-26 11:16:58 +0100

    Small fix

--- S10_S11_markers_for_phylogenetic_reconstruction/scripts/start.sh
+++ S10_S11_markers_for_phylogenetic_reconstruction/scripts/start.sh
@@ -3,6 +3,5 @@
 # Uncompress the file of ENSEMBL gene trees.
 gunzip ../data/Compara.78.protein.nhx.emf.gz 
 
-exit
 # Prepare the zebrafish query sequences for blast searches.
 ruby prepare_queries.rb
