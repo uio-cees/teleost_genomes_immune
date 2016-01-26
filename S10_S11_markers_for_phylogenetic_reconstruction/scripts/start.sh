a4eaf6dc5f69aebe9b1ea21937a64babb231d8b5
Michael Matschiner <michaelmatschiner@mac.com>
2016-01-26 11:14:42 +0100

    Added command to uncompress gene tree file

--- S10_S11_markers_for_phylogenetic_reconstruction/scripts/start.sh
+++ S10_S11_markers_for_phylogenetic_reconstruction/scripts/start.sh
@@ -1,4 +1,8 @@
 # This pipeline requires the following tools (tested versions in parentheses):
 
+# Uncompress the file of ENSEMBL gene trees.
+gunzip ../data/Compara.78.protein.nhx.emf.gz 
+
+exit
 # Prepare the zebrafish query sequences for blast searches.
 ruby prepare_queries.rb
